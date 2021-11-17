3da0072b264bf1a2ef3a05551651caaa2b27e969
Irving Michelle Rios Castellanos <gamma-1994@hotmail.com>
2021-11-17 11:28:52 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -4,9 +4,14 @@ const fs = require("fs");
 const path = require("path");
 const { DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
 });
 const basename = path.basename(__filename);
 
